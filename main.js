50b071c7ae4f6b2cdeeec10ca8ca7c6c31550076
ManabBiswas <156077951+ManabBiswas@users.noreply.github.com>
2024-10-24 22:02:49 +0530

    Update main.js for updated index.html

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function sendMessage() {
 
 }
-
 function toggleTheme() {
     document.body.classList.toggle('dark-mode');
     document.querySelector('header').classList.toggle('dark-mode');
     document.getElementById('chat-container').classList.toggle('dark-mode');
     document.getElementById('chat-window').classList.toggle('dark-mode');
     document.querySelector('footer').classList.toggle('dark-mode');
-    const themeIcon = document.getElementById('theme-icon');
+    
+    const themeIcon = document.getElementById('theme-toggle');
     themeIcon.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
 }
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
